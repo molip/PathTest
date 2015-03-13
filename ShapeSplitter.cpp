511f1bce6576510d216766c6a7f9037c59eb2e21
Jon White <jxxwhite@gmail.com>
2015-03-13 21:28:24 +0000

    Working ShapeSplitter.

--- ShapeSplitter.cpp
+++ ShapeSplitter.cpp
@@ -3,10 +3,13 @@
 
 #include "Shape.h"
 
+#include "Jig/Line2.h"
 #include "Jig/Util.h"
 
 #include <cassert>
 #include <map>
+
+using namespace Jig;
 
 ShapeSplitter::ShapeSplitter(Shape& shape) : m_shape(shape)
 {
@@ -21,9 +24,9 @@ int ShapeSplitter::ChooseConnectVert(int i) const
 {
 	assert(m_shape.GetAngle(i) < 0); // Concave.
 
-	Jig::Vec2 fromPrev = m_shape.GetVec(i - 1, i).Normalised();
-	Jig::Vec2 fromNext = m_shape.GetVec(i + 1, i).Normalised();
-	Jig::Vec2 normal = (fromPrev + fromNext) / 2.0f;
+	Vec2 fromPrev = m_shape.GetVec(i - 1, i).Normalised();
+	Vec2 fromNext = m_shape.GetVec(i + 1, i).Normalised();
+	Vec2 normal = (fromPrev + fromNext) / 2.0f;
 	normal.Normalise();
 
 	double angleToPrev = normal.GetAngle(-fromPrev);
@@ -31,37 +34,35 @@ int ShapeSplitter::ChooseConnectVert(int i) const
 
 	assert(angleToPrev >= 0 && angleToNext <= 0);
 
+	// Get candidate verts, best angle first. 
 	std::map<double, int> map;
 	for (int j = i + 2; j < i + (int)m_shape.size() - 1; ++j) // Non-adjacent to i.
 	{
-		Jig::Vec2 iToJ = m_shape.GetVec(i, j).Normalised();
+		Vec2 iToJ = m_shape.GetVec(i, j).Normalised();
 		const double angle = normal.GetAngle(iToJ);
 		if (angle < angleToPrev && angle > angleToNext)
-			map.insert(std::make_pair(angle, m_shape.ClampVertIndex(j)));
+			map.insert(std::make_pair(std::fabs(angle), m_shape.ClampVertIndex(j)));
 	}
-
-	double minAngle = 10.0f; // > 2 * pi.
-	int minAngleVert = -1;
-
-	auto Try = [&](const std::pair<double, int>& pair) // Returns true to stop.
-	{
-		if (std::fabs(pair.first) < minAngle)
-		{
-			minAngle = std::fabs(pair.first);
-			minAngleVert = pair.second;
-		}
-		return m_shape.GetAngle(pair.second) < 0; // Stop if we've hit a concave vertex.
-	};
 
 	for (auto& pair : map)
 	{
-		if (Try(pair))
+		const int vert = pair.second;
+		Line2 line = Line2::MakeFinite(m_shape.GetVertex(i), m_shape.GetVertex(vert));
+
+		bool hit = false;
+
+		for (int j = i + 1; j < i + (int)m_shape.size() - 1; ++j) // Non-adjacent to i.
 		{
-			for (auto& pair : Util::Reverse(map)) // Try the other way, might get closer to normal.
-				if (Try(pair))
+			if (m_shape.ClampVertIndex(j) != vert && m_shape.ClampVertIndex(j + 1) != vert) // Ignore edges including vert. 
+			{
+				Line2 edge = Line2::MakeFinite(m_shape.GetVertex(j), m_shape.GetVertex(j + 1));
+				if (hit = edge.Intersect(line))
 					break;
-			break;
+			}
 		}
+		if (!hit)
+			return vert;
 	}
-	return minAngleVert;
+	
+	return -1;
 }
